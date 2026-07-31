6e8d25560343c337538225a33bab5d959311823a
kWRizzz <krishnathey2j@gmail.com>
2026-07-31 23:47:49 +0530

    fix done

--- frontend/src/components/interviews/RunTime.jsx
+++ frontend/src/components/interviews/RunTime.jsx
@@ -24,7 +24,7 @@ const RunTime = () => {
     }
     return (
         <button
-            className="bg-green-600 text-white px-4 py-2 rounded disabled:opacity-50"
+            className="bg-green-600  text-white px-4 py-2 rounded disabled:opacity-50"
             disabled={isRunning}
             onClick={handleSumbit}
         >
